1444b678d1f13e60c062db044a1b7d4964cd7fbf
SayHi丶晴天 <syh007af@gmail.com>
2018-09-04 23:51:55 +0800

    ss

--- DataStructure/SortAlgorithms.py
+++ DataStructure/SortAlgorithms.py
@@ -22,11 +22,11 @@ class ComparisonSort(SortAlgorithm):
         """
         // 分类 -------------- 内部比较排序
         // 数据结构 ---------- 数组
-        // 最差时间复杂度 ---- O(n^2)
-        // 最优时间复杂度 ---- O(n)
-        // 平均时间复杂度 ---- O(n^2)
-        // 所需辅助空间 ------ O(1)
-        // 稳定性 ------------ 稳定
+        // 最差时间复杂度 ---- O()
+        // 最优时间复杂度 ---- O()
+        // 平均时间复杂度 ---- O()
+        // 所需辅助空间 ------ O()
+        // 稳定性 ------------ 
         """
         for i in range(self.length - 1):
             for j in range(0, self.length - 1 - i):
@@ -34,23 +34,77 @@ class ComparisonSort(SortAlgorithm):
                     self.swap(j, j + 1)
 
     def SelectionSort(self):
-        pass
+        """
+        // 分类 -------------- 内部比较排序
+        // 数据结构 ---------- 数组
+        // 最差时间复杂度 ---- O()
+        // 最优时间复杂度 ---- O()
+        // 平均时间复杂度 ---- O()
+        // 所需辅助空间 ------ O()
+        // 稳定性 ------------ 
+        """
+        for i in range(self.length - 1):
+            k = i
+            for j in range(i + 1, self.length):
+                if self.L[j] < self.L[k]:
+                    k = j
+            if k != i:
+                self.swap(i, k)
 
     def InsertionSort(self):
+        """
+        // 分类 -------------- 内部比较排序
+        // 数据结构 ---------- 数组
+        // 最差时间复杂度 ---- O()
+        // 最优时间复杂度 ---- O()
+        // 平均时间复杂度 ---- O()
+        // 所需辅助空间 ------ O()
+        // 稳定性 ------------ 
+        """
         for i in range(1, self.length):
             for j in range(0, i):
                 if self.L[i] < self.L[j]:
-                    for k in range(j, i, -1):
+                    for k in range(i, j, -1):
                         self.swap(k, k - 1)
-                    print(self.L)
                     break
 
 
     def MergeSort(self):
+        """
+        // 分类 -------------- 内部比较排序
+        // 数据结构 ---------- 数组
+        // 最差时间复杂度 ---- O()
+        // 最优时间复杂度 ---- O()
+        // 平均时间复杂度 ---- O()
+        // 所需辅助空间 ------ O()
+        // 稳定性 ------------ 
+        """
         pass
 
-    def QuickSort(self):
-        pass
+    def QuickSort(self, L):
+        """
+        // 分类 -------------- 内部比较排序
+        // 数据结构 ---------- 数组
+        // 最差时间复杂度 ---- O()
+        // 最优时间复杂度 ---- O()
+        // 平均时间复杂度 ---- O()
+        // 所需辅助空间 ------ O()
+        // 稳定性 ------------ 
+        """
+        i = 0
+        j = len(L) - 1
+        if len(L) > 1:
+            k = L[i]
+            while i < j:
+                while L[j] >= k and i < j:
+                    j -= 1
+                L[i], L[j] = L[j], L[i]
+                while L[i] <= k and i < j:
+                    i += 1
+                L[i], L[j] = L[j], L[i]
+            return self.QuickSort(L[:i]) + [k] + self.QuickSort(L[i+1:])
+        else:
+            return L
 
     def HeapSort(self):
         pass
@@ -59,6 +113,6 @@ class ComparisonSort(SortAlgorithm):
 if __name__ == "__main__":
     test_list = [2, 4, 3, 6, 33, 2, 7, 8, 9]
     cs = ComparisonSort(test_list)
-    cs.InsertionSort()
+    print(cs.QuickSort(test_list))
     print(cs.L)
         
